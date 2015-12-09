1f23c03e25079d464a790e56c376042f8a32117b
Tomislav Capan <tomislav.capan@gmail.com>
2015-12-09 23:51:38 +0100

    Usage sample code added; Readme tweak

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # Scout HTTP Cache
 
-Scout HTTP Cache is an HTTP client caching module, providing out-of-the-box
-caching layer for outgoing HTTP calls to external APIs.
-
-It's HTTP client agnostic, for clients following the common request and
-response signatures, like [Request](https://github.com/request/request)
-or [Wreck](https://github.com/hapijs/wreck).
+Scout HTTP Cache is a Node.js HTTP client caching module, providing
+out-of-the-box caching layer for outgoing HTTP calls to external APIs.
 
 It leverages [Catbox](https://github.com/hapijs/catbox) module from hapi
-ecosystem and [Catbox-Redis](https://github.com/hapijs/catbox-redis) caching
-strategy Redis adapter.
+ecosystem with [Catbox-Redis](https://github.com/hapijs/catbox-redis)
+caching strategy adapter for Redis, and is HTTP client agnostic, supporting
+[Request](https://github.com/request/request) and [Wreck](https://github.com/hapijs/wreck)
+clients, but also any other which is using the same request and response
+signatures.
---
+++ example/index.js
@@ -0,0 +1,50 @@
+var Promise   = require('bluebird')
+  , httpCache = require('../lib')
+  , wreck     = require('wreck')
+  , request   = require('request')
+  , cacheReady;
+
+cacheReady = httpCache.init({ partition: 'examplehttpcache' });
+
+cacheReady.then(function () {
+  var getUrl       = 'http://jsonplaceholder.typicode.com/posts/1'
+    , postUrl      = 'http://jsonplaceholder.typicode.com/posts'
+    , httpOptions  = { json: true, /*timeout: 1000, maxRetries: 3,*/ body: { title: 'test' } }
+    , cacheOptions = { refreshAfter: 15 * 1000, ttl: 24 * 60 * 60 * 1000 }
+    , getPromise
+    , postPromise;
+
+
+  Promise.all([
+    new Promise(function (resolve, reject) {
+      var options = {
+        method      : 'POST',
+        url         : postUrl,
+        httpClient  : wreck,
+        httpOptions : httpOptions,
+        cacheOptions: cacheOptions,
+      };
+
+      httpCache.request(options, function (err, body) {
+        if (err) { console.log('POST ERROR:', err); }
+        console.log(body);
+        resolve();
+      });
+    }),
+    new Promise(function (resolve, reject) {
+      var options = {
+        method      : 'GET',
+        url         : getUrl,
+        httpClient  : wreck,
+        httpOptions : httpOptions,
+        cacheOptions: cacheOptions,
+      };
+
+      httpCache.request(options, function (err, body) {
+        if (err) { console.log('GET ERROR:', err); }
+        console.log(body);
+        resolve();
+      });
+    }),
+  ]).then(httpCache.stopCache);
+});
